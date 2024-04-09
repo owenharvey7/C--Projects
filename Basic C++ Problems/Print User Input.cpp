e0c6f51fe6ad235ddd7a9e2f3e8cf956a2806082
Owen Harvey <80526926+owenharvey7@users.noreply.github.com>
2024-04-09 10:24:24 -0400

    Add files via upload

---
+++ Basic C++ Problems/Print User Input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Your number is: " << num << endl;
+}
